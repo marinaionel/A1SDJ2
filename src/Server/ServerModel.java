bb5f551891c5e6e9836cfa2dba00da829d1c5d1f
Konrad Piotrowski <konrad.piotrowski00@gmail.com>
2019-03-08 12:27:48 +0100

    Building Server - work in progress

--- src/Server/ServerModel.java
+++ src/Server/ServerModel.java
@@ -1,4 +1,11 @@
 package Server;
 
 public class ServerModel {
+
+    
+
+
+
+
+
 }
--- src/Server/SocketServer.java
+++ src/Server/SocketServer.java
@@ -1,4 +1,34 @@
 package Server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class SocketServer {
+
+    private SSocketHandler socketHandler;
+
+    private ArrayList<Socket> sockets;
+
+
+    public SocketServer(SSocketHandler socketHandler)
+    {
+        this.socketHandler = socketHandler;
+        sockets = new ArrayList<>();
+        socketHandler.setServer(this);
+    }
+
+    public void addClient(Socket socket)
+    {
+        sockets.add(socket);
+    }
+
+    public int getNoPlayers()
+    {
+        return sockets.size();
+    }
+
+
+
+
 }
